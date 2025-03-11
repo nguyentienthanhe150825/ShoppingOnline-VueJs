ccfe9b36beef376c8f3694b111f629f7c09a3188
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2025-03-11 09:36:12 +0700

    Setting Ant Design Vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css' // Import CSS của Ant Design Vue
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Antd) // Kích hoạt Ant Design Vue
+app.mount('#app')
